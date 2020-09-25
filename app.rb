cccf4990a18345945a20b1dd26621f77adcbf24a
Keesha Hargrove <kharg14@gmail.com>
2020-09-25 20:51:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,8 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
-  
+  get '/medicines' do
+  @medicines = Medicine.all
+ 
+  erb :'medicines/index.html.erb'
+end
   
   
   

dccfd2cb361a496f50e4d0471bd466b78a440716
Keesha Hargrove <kharg14@gmail.com>
2020-09-25 21:01:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,16 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  GET '/name' do
+  get '/name' do
 "My name is __" 
 
 end
- GET '/hometown' do
+ get '/hometown' do
     "My hometown is __"  
  end
   
   
-  GET '/favorite-song' do
+  get '/favorite-song' do
     "My favorite-song is __"  
  end
   
